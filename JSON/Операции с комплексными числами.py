3662548938fddee12ed848f1e237d684f55ee418
ChingisB <c_bogdatov@kbtu.kz>
2022-02-16 19:18:35 +0600

    upd

--- JSON/Операции с комплексными числами.py
+++ JSON/Операции с комплексными числами.py
@@ -1,4 +1,3 @@
-from base64 import encode
 import json
 
 
@@ -7,5 +6,7 @@ with open('in.json', encoding='utf8') as infile, open('out.json', 'w', encoding=
     ans = {"complex": []}
     ans['complex'].append({"Re": data[0]['Re'] + data[1]['Re'], 'Im': data[0]['Im'] + data[1]['Im']})
     ans['complex'].append({"Re": data[0]['Re'] - data[1]['Re'], 'Im': data[0]['Im'] - data[1]['Im']})
-    ans['complex'].append({"Re": data[0]['Re'] * data[1]['Re'] - data[0]['Im'] * data[1]['Im'], 'Im': data[0]['Re'] * data[1]['Im'] + data[0]['Im'] * data[1]['Re']})
+    t1 = data[0]['Re'] * data[1]['Re'] - data[0]['Im'] * data[1]['Im']
+    t = {"Re": t1, 'Im': data[0]['Re'] * data[1]['Im'] + data[0]['Im'] * data[1]['Re']}
+    ans['complex'].append(t)
     json.dump(ans, out)
