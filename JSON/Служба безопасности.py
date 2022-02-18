38f92a520ee546d6d23522cf49baf11b6782f950
ChingisB <c_bogdatov@kbtu.kz>
2022-02-18 18:41:29 +0600

    upd

--- JSON/Служба безопасности.py
+++ JSON/Служба безопасности.py
@@ -10,20 +10,22 @@ with open('regions.csv', encoding='utf8') as infile2:
 
 
 def check_security(name, place):
-    if name not in payers.keys():
+    payer = filter(lambda x: (x['lastname'] == name[0], x['firstname'] == name[1], x['middlename'] == name[2]))
+    if not list(payer):
         return (None, None)
+    payer = list(payer[0])
     ans1 = 0
     ans2 = 0
-    if str(payers[name][:2]) != str(regions[place]):
+    if str(payer['tin'][:2]) != str(regions[place]):
         ans1 = -1
     cnt1 = 0
     coefs1 = '7, 2, 4, 10, 3, 5, 9, 4, 6, 8'.split(', ')
     for i in range(10):
-        cnt1 = int(coefs1) * int(payers[name][i])
+        cnt1 = int(coefs1) * int(payer['tin'][:2])
     cnt2 = 0
     coefs2 = '3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8'.split(', ')
     for i in range(11):
-        cnt2 = int(coefs1) * int(payers[name][i])
+        cnt2 = int(coefs1) * int(payer['tin'][:2])
     if cnt1 != cnt2:
         ans2 = -1
     return (ans1, ans2)
