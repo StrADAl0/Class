9c5ac6c87e4165973fa659bf7ce5d6b869267bef
ChingisB <c_bogdatov@kbtu.kz>
2022-02-18 18:21:34 +0600

    test

---
+++ JSON/Угадай слово.py
@@ -0,0 +1,25 @@
+import json
+import random
+
+n = int(input())
+with open('words.json', encoding='utf8') as infile:
+    word = random.choice(infile.load()[n])
+
+
+game = True
+cipher = '*' * n
+print('Your word {}. Enter the letter.'.format(cipher))
+while game:
+    letter = input()
+    if not letter.isalpha():
+        print('Bad input. Try once more, please.')
+        continue
+    if letter not in word:
+        print('Your word {}. Enter the letter.'.format(cipher))
+        continue
+    for i in range(n):
+        if word[i] == letter:
+            cipher = cipher[:i] + letter + cipher[i + 1:]
+
+
+
