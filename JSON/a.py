0558cd1df8a04cd86cef899c1aa9c9c61eb10283
ChingisB <c_bogdatov@kbtu.kz>
2022-02-21 19:02:44 +0600

    upd

--- JSON/a.py
+++ JSON/a.py
@@ -1,11 +1,9 @@
 import json
 import csv
 
-
-coefs1 = '7, 2, 4, 10, 3, 5, 9, 4, 6, 8'.split(', ')
-coefs2 = '3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8'.split(', ')
-
 def check_security(name, place):
+    coefs1 = '7, 2, 4, 10, 3, 5, 9, 4, 6, 8'.split(', ')
+    coefs2 = '3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8'.split(', ')
     with open('taxpayer_in.json', encoding='utf8') as infile1:
         payers = json.load(infile1)
     with open('regions.csv', encoding='utf8') as infile2:
