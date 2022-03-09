5292ec24a1d1a2395827dd0661af13aeabd317b4
ChingisB <c_bogdatov@kbtu.kz>
2022-03-09 19:01:15 +0600

    upd

---
+++ poli/Заметки.py
@@ -0,0 +1,71 @@
+class ShoppingList:
+    def __init__(self, *args):
+        self.list = args
+    
+    def append(self, value):
+        self.list.append((value, False))
+    
+    def values(self):
+        return self.list
+    
+    def check_value(self, value):
+        for i in filter(lambda x: x[0] == value, self.list):
+            if i[0] == value:
+                i[1] = True
+                break
+    
+    def checked_values(self):
+        return list(filter(lambda x: x[1], self.list))
+    
+    def rest_values(self):
+        return list(filter(lambda x: not x[1], self.list))
+
+
+class TODOList:
+    def __init__(self, *args):
+        self.list = sorted(args, key=lambda x: x[2], reverse=True)
+    
+    def append(self, value, urgency):
+        self.list.append((value, urgency, False))
+    
+    def values(self):
+        return self.list
+    
+    def check_value(self, value):
+        for i in filter(lambda x: x[0] == value, self.list):
+            if i[0] == value:
+                i[2] = True
+                break
+    
+    def checked_values(self):
+        return list(filter(lambda x: x[2], self.list))
+    
+    def rest_values(self):
+        return list(filter(lambda x: not x[2], self.list))
+
+
+class Route:
+    def __init__(self, *args):
+        self.list = args
+    
+    def append(self, value, time):
+        if int(self.list[-1].split(':')[0]) < int(time.split(':')[0]):
+            self.values.append((value, time, False))
+        elif int(self.list[-1].split(':')[0]) == int(time.split(':')[0]):
+            if int(self.list[-1].split(':')[1]) < int(time.split(':')[1]):
+                self.values.append((value, time, False))
+    
+    def values(self):
+        return self.list
+    
+    def check_value(self, value):
+        for i in filter(lambda x: x[0] == value, self.list):
+            if i[0] == value:
+                i[2] = True
+                break
+    
+    def checked_values(self):
+        return list(filter(lambda x: x[2], self.list))
+    
+    def rest_values(self):
+        return list(filter(lambda x: not x[2], self.list))
