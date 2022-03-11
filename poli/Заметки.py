18db067b729435d73b866d6071a8023f223c92aa
ChingisB <c_bogdatov@kbtu.kz>
2022-03-11 18:43:40 +0600

    upd

--- poli/Заметки.py
+++ poli/Заметки.py
@@ -50,10 +50,10 @@ class Route:
         self.list = list(args)
     
     def append(self, value, time):
-        if int(self.list[-1].split(':')[0]) < int(time.split(':')[0]):
+        if int(self.list[-1][1].split(':')[0]) < int(time.split(':')[0]):
             self.list.append((value, time, False))
-        elif int(self.list[-1].split(':')[0]) == int(time.split(':')[0]):
-            if int(self.list[-1].split(':')[1]) < int(time.split(':')[1]):
+        elif int(self.list[-1][1].split(':')[0]) == int(time.split(':')[0]):
+            if int(self.list[-1][1].split(':')[1]) < int(time.split(':')[1]):
                 self.list.append((value, time, False))
     
     def values(self):
@@ -70,3 +70,15 @@ class Route:
     
     def rest_values(self):
         return list(filter(lambda x: not x[2], self.list))
+
+
+path = Route(("Stratford", "12:15", False),
+             ("Hackney Central", "12:24", False),
+             ("Dalston Kingsland", "12:36", False))
+path.append("Camden Road", "12:45")
+path.append("Richmond", "11:52")
+print(*path.values(), sep="\n")
+print()
+path.check("Stratford")
+path.check("Hackney Central")
+print(*[x[0] for x in path.rest_values()], sep="\n")
