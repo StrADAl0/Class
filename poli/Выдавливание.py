dccfdf8d3ad99e2692aa1b32aaa533cc7c89d803
ChingisB <c_bogdatov@kbtu.kz>
2022-03-11 18:10:53 +0600

    upd

---
+++ poli/Выдавливание.py
@@ -0,0 +1,25 @@
+from math import pi
+
+
+class Square:
+    def __init__(self, side):
+        self.area = side ** 2
+    
+    def extrude(self, h):
+        return self.area * h
+
+
+class Triangle:
+    def __init__(self, side):
+        self.area = (3 ** 0.5) * (side ** 2) / 4
+    
+    def extrude(self, h):
+        return self.area * h
+
+
+class Circle:
+    def __init__(self, radius):
+        self.area = pi * (radius ** 2)
+    
+    def extrude(self, h):
+        return self.area * h
