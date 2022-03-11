0729e64435c671c46275e121be11fe4b8690ccc1
ChingisB <c_bogdatov@kbtu.kz>
2022-03-11 18:20:33 +0600

    upd

--- poli/Выдавливание.py
+++ poli/Выдавливание.py
@@ -9,6 +9,13 @@ class Square:
         return self.area * h
 
 
+class Rectangle:
+    def __init__(self, side1, side2):
+        self.area = side1 * side2
+    
+    def extrude(self, h):
+        return self.area * h 
+
 class Triangle:
     def __init__(self, side):
         self.area = (3 ** 0.5) * (side ** 2) / 4
