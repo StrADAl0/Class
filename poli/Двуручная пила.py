eb155b04def706ff6f52d75f807a756ea4bffea5
ChingisB <c_bogdatov@kbtu.kz>
2022-03-11 19:04:46 +0600

    upd

--- poli/Двуручная пила.py
+++ poli/Двуручная пила.py
@@ -1,3 +1,6 @@
+from itertools import zip_longest
+
+
 class TwoHandedSawUp:
     def __init__(self, values):
         self.values = values
@@ -6,7 +9,7 @@ class TwoHandedSawUp:
         ans = []
         self.values.sort()
         len_s = int(len(self.values) / 2)
-        for i, j in zip(self.values[:len_s], self.values[len_s:]):
+        for i, j in zip_longest(self.values[:len_s], self.values[len_s:]):
             if i is not None:
                 ans.append(i)
             if j is not None:
@@ -25,7 +28,7 @@ class TwoHandedSawDown:
         ans = []
         self.values.sort(reverse=True)
         len_s = int(len(self.values) / 2)
-        for i, j in zip(self.values[:len_s], self.values[len_s:]):
+        for i, j in zip_longest(self.values[:len_s], self.values[len_s:]):
             if i is not None:
                 ans.append(i)
             if j is not None:
@@ -34,3 +37,20 @@ class TwoHandedSawDown:
     
     def get_list(self):
         return self.values
+
+
+def print_hist(array):
+    for i in array:
+        print('*' * i)
+
+
+arr = [8, 6, 1, 2, 7, 4, 5, 3, 9]
+thsu = TwoHandedSawUp(arr)
+thsu.sawing()
+print(*thsu.get_list())
+print_hist(thsu.get_list())
+
+thsd = TwoHandedSawDown(arr)
+thsd.sawing()
+print(*thsd.get_list())
+print_hist(thsd.get_list())
